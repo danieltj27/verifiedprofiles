a12b8cc710bdea5a2fe9f9eca1fd1fe273fbf6db
Daniel James <danielt70@icloud.com>
2026-04-06 23:38:11 +0100

    Improve the doc blocks of special functions and remove unused code

--- includes/functions.php
+++ includes/functions.php
@@ -8,16 +8,10 @@
 
 namespace danieltj\verifiedprofiles\includes;
 
-use phpbb\auth\auth;
 use phpbb\config\config;
 use phpbb\db\driver\driver_interface as database;
 
 final class functions {
-
-	/**
-	 * @var auth
-	 */
-	protected $auth;
 
 	/**
 	 * @var config
@@ -32,87 +26,36 @@ final class functions {
 	/**
 	 * Constructor
 	 */
-	public function __construct( auth $auth, config $config, database $database ) {
+	public function __construct( config $config, database $database ) {
 
-		$this->auth = $auth;
 		$this->config = $config;
 		$this->database = $database;
 
 	}
 
 	/**
-	 * Check the user's verification status.
+	 * Returns whether the user is verified or not.
 	 *
-	 * @param  integer $user_id      The user ID to check against.
-	 * @param  boolean $check_groups Flag to check user's groups for verification.
-	 * @return boolean               The user's verification status (is or is not).
-	 */
-	public function is_user_verified( $user_id = 0, $check_groups = true ) {
-
-		if ( 0 === $user_id ) {
-
-			return false;
-
-		}
-
-		$sql = 'SELECT user_verified FROM ' . USERS_TABLE . ' WHERE ' . $this->database->sql_build_array( 'SELECT', [
-			'user_id' => $user_id,
-			'user_verified' => 1
-		] );
-
-		$result = $this->database->sql_query( $sql );
-		$user = $this->database->sql_fetchrow( $result );
-		$this->database->sql_freeresult( $result );
-
-		if ( ! empty( $user ) ) {
-
-			return true;
-
-		}
-
-		if ( true === $check_groups && $this->is_in_verified_group( $user_id ) ) {
-
-			return true;
-
-		}
-
-		return false;
-
-	}
-
-	/**
-	 * Check if the user has hidden their badge.
+	 * @param  integer $user_id The user ID to check if they are verified.
 	 * 
-	 * @param  integer $user_id The user ID to check against.
-	 * @return boolean          The badge visibility status (is or is not).
+	 * @return boolean  True if user is verified, false if not.
 	 */
-	public function is_badge_hidden( $user_id = 0 ) {
+	public function is_user_verified( $user_id ) {
 
-		if ( 0 === (int) $user_id ) {
+		$user_id = (int) $user_id;
 
-			return true;
+		$result = $this->database->sql_query( 'SELECT user_verified FROM ' . USERS_TABLE . ' WHERE ' .
+			$this->database->sql_build_array( 'SELECT', [
+				'user_id'		=> $user_id,
+				'user_verified'	=> 1,
+			]
+		) );
 
-		}
-
-		$sql = 'SELECT * FROM ' . USERS_TABLE . ' WHERE ' . $this->database->sql_build_array( 'SELECT', [
-			'user_id' => (int) $user_id
-		] );
-
-		$result = $this->database->sql_query( $sql );
 		$user = $this->database->sql_fetchrow( $result );
+		
 		$this->database->sql_freeresult( $result );
 
-		// Create new auth object for specific user.
-		$user_auth = new auth();
-		$user_auth->acl( $user );
-
-		if ( ! $user_auth->acl_get( 'u_hide_verified_badge' ) ) {
-
-			return false;
-
-		}
-
-		if ( ! empty( $user ) && '1' === $user[ 'user_verify_visibility' ] ) {
+		if ( false === $user ) {
 
 			return false;
 
@@ -123,114 +66,98 @@ final class functions {
 	}
 
 	/**
-	 * Check the group's verification status.
-	 *
-	 * @param  integer $group_id The group ID to check against.
-	 * @return boolean           The group's verification status (is or is not).
+	 * Returns whether a group auto verifies it's members.
+	 * 
+	 * @param  integer $group_id The group ID to check for verification.
+	 * 
+	 * @return boolean  True if groupdoes verify members, false if not.
 	 */
-	public function is_group_verified( $group_id = 0 ) {
+	public function is_group_verified( $group_id ) {
 
-		if ( 0 === (int) $group_id ) {
+		$group_id = (int) $group_id;
 
-			return false;
+		$result = $this->database->sql_query( 'SELECT group_verified FROM ' . GROUPS_TABLE . ' WHERE ' .
+			$this->database->sql_build_array( 'SELECT', [
+				'group_id'			=> $group_id,
+				'group_verified'	=> 1,
+			]
+		) );
 
-		}
-
-		$sql = 'SELECT group_verified FROM ' . GROUPS_TABLE . ' WHERE ' . $this->database->sql_build_array( 'SELECT', [
-			'group_id' => (int) $group_id,
-			'group_verified' => 1
-		] );
-
-		$result = $this->database->sql_query( $sql );
 		$group = $this->database->sql_fetchrow( $result );
+		
 		$this->database->sql_freeresult( $result );
 
-		if ( ! empty( $group ) ) {
+		if ( false === $group ) {
 
-			return true;
+			return false;
 
 		}
 
-		return false;
+		return true;
 
 	}
 
 	/**
-	 * Check if the user is in a verified group.
+	 * Returns whether a user has hidden their badge.
 	 * 
-	 * @param  integer $user_id The user ID to check against.
-	 * @return boolean          The group verification status (is or is not).
+	 * @param  integer $user_id The user ID to check their badge visibility.
+	 * 
+	 * @return boolean  True if the badge is hidden, false if not.
 	 */
-	public function is_in_verified_group( $user_id = 0 ) {
+	public function is_badge_hidden( $user_id ) {
 
-		if ( 0 === $user_id ) {
+		$user_id = (int) $user_id;
 
-			return false;
+		$result = $this->database->sql_query( 'SELECT * FROM ' . USERS_TABLE . ' WHERE ' .
+			$this->database->sql_build_array( 'SELECT', [
+				'user_id'		=> $user_id,
+				'user_verified'	=> 1,
+			]
+		) );
 
-		}
-
-		$sql = 'SELECT group_id FROM ' . USER_GROUP_TABLE . ' WHERE ' . $this->database->sql_build_array( 'SELECT', [
-			'user_id' => (int) $user_id
-		] );
-
-		$result = $this->database->sql_query( $sql );
-		$users_groups = $this->database->sql_fetchrowset( $result );
+		$user = $this->database->sql_fetchrow( $result );
+		
 		$this->database->sql_freeresult( $result );
 
-		if ( empty( $users_groups ) ) {
+		if ( false === $user ) {
 
 			return false;
 
 		}
 
-		$group_ids = [];
+		$new_auth = new \phpbb\auth\auth();
+		$new_auth->acl( $user );
 
-		foreach ( $users_groups as $group ) {
-
-			$group_ids[] = (int) $group[ 'group_id' ];
-
-		}
-
-		$sql = 'SELECT group_verified FROM ' . GROUPS_TABLE . ' WHERE ' . $this->database->sql_in_set( 'group_id', $group_ids );
-
-		$result = $this->database->sql_query( $sql );
-		$groups = $this->database->sql_fetchrowset( $result );
-		$this->database->sql_freeresult( $result );
-
-		if ( empty( $groups ) ) {
+		if ( ! $new_auth->acl_get( 'u_hide_verified_badge' ) ) {
 
 			return false;
 
 		}
 
-		foreach ( $groups as $group ) {
+		if ( 1 !== (int) $user[ 'user_verify_visibility' ] ) {
 
-			if ( '1' === $group[ 'group_verified' ] ) {
-
-				return true;
-
-			}
+			return false;
 
 		}
 
-		return false;
+		return true;
 
 	}
 
 	/**
-	 * Check if a custom verification badge has been uploaded.
+	 * Return the URL of a custom verification badge.
 	 * 
-	 * @param  boolean        $strict    Check the database and file system for a custom badge or
-	 *                                   just check the database for the file name. Setting this to
-	 *                                   true is strongly advised.
-	 * @return boolean|string $badge_url Returns false if there isn't a custom badge uploaded or can return
-	 *                                   a string is a custom badge has been uploaded.
+	 * @param boolean $strict (optional) A flag used to decided whether to check just the
+	 *                        database or the file system as well for the presence of a
+	 *                        custom badge. It's strongly advised that this is set to true.
+	 * 
+	 * @return boolean|string $url The URL of the custom badge or false if one is not set.
 	 */
-	public function has_custom_badge( $strict = false ) {
+	public function has_custom_badge( $strict = true ) {
 
 		global $phpbb_root_path;
 
-		// Create the default file location for custom badges.
+		// Default location for custom badges.
 		$file_location = $phpbb_root_path . 'images';
 
 		if ( true === $strict ) {
@@ -243,7 +170,6 @@ final class functions {
 
 		}
 
-		// Create a full path to the custom badge.
 		$badge_url = generate_board_url() . '/images/' . $this->config[ 'verified_profiles_custom_badge' ];
 
 		return $badge_url;
@@ -251,11 +177,11 @@ final class functions {
 	}
 
 	/**
-	 * Check if the current page has badges enabled on it.
+	 * Returns whether a verification badge is enabled for this page.
 	 * 
-	 * @param  string  $current_page The page to check if badges are enabled.
-	 * @return boolean               Either true or false. True if badges can be displayed
-	 *                               and false if badges cannot be displayed.
+	 * @param string $page The page to check if badges are enabled.
+	 * 
+	 * @return boolean  True if enabled or false if not.
 	 */
 	public function is_location_enabled( $current_page = '' ) {
 
