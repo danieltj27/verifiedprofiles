0dabe78939fb29f255c3ff403d8e72786d2e1f7d
Daniel James <danielt70@icloud.com>
2026-05-15 10:37:21 +0100

    Update min required versions

--- ext.php
+++ ext.php
@@ -17,7 +17,7 @@ class ext extends \phpbb\extension\base {
 
 		$config = $this->container->get( 'config' );
 
-		return phpbb_version_compare( $config[ 'version' ], '3.3.0', '>=' ) && phpbb_version_compare( PHP_VERSION, '7.4.0', '>=' );
+		return phpbb_version_compare( $config[ 'version' ], '3.3.0', '>=' ) && phpbb_version_compare( PHP_VERSION, '8.0.0', '>=' );
 
 	}
 
